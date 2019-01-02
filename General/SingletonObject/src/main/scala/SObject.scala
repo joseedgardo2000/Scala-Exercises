c54f18376eb5b47b03ba4c5c59183e18d6251542
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-02 16:32:42 -0600

    SingletonObject - Init
    SingletonObject - Init

---
+++ General/SingletonObject/src/main/scala/SObject.scala
@@ -0,0 +1,11 @@
+
+
+object SObject {
+  def printMessage01():Unit = {
+      println("This is Message01")
+  }
+  
+  def printMessage(message:String):Unit = {
+      println(message)
+  }
+}
---
+++ General/SingletonObject/src/main/scala/SingletonObject.scala
@@ -0,0 +1,10 @@
+
+
+object SingletonObject {
+  def main(args:Array[String]):Unit = {
+      println("------------- SingletonObject -------------")
+      SObject.printMessage("Hi!!! How are you??")
+      SObject.printMessage01()
+      println("------------- --------------- -------------")
+  }
+}
