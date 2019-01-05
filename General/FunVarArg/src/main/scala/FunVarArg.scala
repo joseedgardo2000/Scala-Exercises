4cdb1e4f53bca533c1aece93653441b5a6d4fd35
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-05 12:24:41 -0600

    FunVarArg - Init
    FunVarArg - Init

---
+++ General/FunVarArg/src/main/scala/FunVarArg.scala
@@ -0,0 +1,25 @@
+
+
+object FunVarArg {
+  def main(arg:Array[String]):Unit = {
+      val l=List("cat", "dog", "horse")
+      val l2=List(11,22,33,44,55)
+      println("---------------- FunVarArg ----------------")
+      printStrings("hi","hello", "Good Morning", "Bye")
+      println("...........")
+      printInts(1,2,3,4,5)
+      println("...........")
+      printStrings(l: _*)
+      println("...........")
+      printInts(l2: _*)
+      println("-------------------------------------------")
+  }
+  
+  def printInts(ints:Int*){
+      for(i <- ints) println(i)
+  }
+  
+  def printStrings(strs:String*){
+      strs.foreach(println)
+  }
+}
