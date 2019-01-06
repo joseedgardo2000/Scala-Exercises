45854ca487ad9c83a9481d5268b2644511a51b75
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-05 18:49:10 -0600

    Closures - Init
    Closures - Init

---
+++ General/Closures/src/main/scala/Closures.scala
@@ -0,0 +1,37 @@
+import farscope.ClasA
+import farscope.ClasA
+
+object Closures {
+  var hi = ""
+  var greet = ""
+  def main(args:Array[String]):Unit = {
+      println("------------------ Closures ------------------")
+      closure01()
+      closure02()
+      println("------------------ -------- ------------------")
+      
+  }
+  //-----------------------------------------------------------
+  def closure02():Unit = {
+      val cla = new ClasA()
+      hi = "Hello"
+      greet = "good morning"
+      cla.exec(sayHello, "Marry")
+      hi = "Hola"
+      greet = "buenos dias"
+      cla.exec(sayHello, "Maria")
+  }
+  
+  def sayHello(name:String):String = {
+      s"${hi} ${name}, ${greet}"
+  }
+  
+  //-----------------------------------------------------------
+  def closure01():Unit = {
+      val num1 = 10
+      val sum = (i:Int)=> i + num1
+      println(sum(5))
+  }
+  
+  
+}
---
+++ General/Closures/src/main/scala/FarScope.scala
@@ -0,0 +1,7 @@
+package farscope
+
+class ClasA{
+    def exec(f:(String)=>String, name:String){
+        println(f(name))
+    }
+}
