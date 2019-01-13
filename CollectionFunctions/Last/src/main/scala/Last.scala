36e59b5e30e8893ae54b78d52f2ec72e5db5ae90
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-13 02:58:25 -0600

    Update Last.scala

--- CollectionFunctions/Last/src/main/scala/Last.scala
+++ CollectionFunctions/Last/src/main/scala/Last.scala
@@ -4,9 +4,11 @@ object Last {
   def main(args:Array[String]):Unit = {
     println("====================== Last ======================")
     val s1 = Seq("pig", "horse", "cow", "monkey", "fly")
+    val s2 = Seq()
     println(s1)
     println(s"s1(s1.size-1): ${s1(s1.size-1)}")
     println(s"s1.last: ${s1.last}")
+    
     println("====================== ====== ======================")
   }
 }
