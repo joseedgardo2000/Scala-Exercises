c10ea4570306aeaf1092cab2af130afdb3568ace
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-13 02:58:20 -0600

    Update Head.scala

--- CollectionFunctions/Head/src/main/scala/Head.scala
+++ CollectionFunctions/Head/src/main/scala/Head.scala
@@ -4,9 +4,12 @@ object Head {
   def main(args:Array[String]):Unit = {
     println("====================== Head ======================")
     val s1 = Seq("pig", "horse", "cow", "monkey", "fly")
+    val s2 = Seq()
     println(s1)
     println(s"s1(0): ${s1(0)}")
     println(s"s1.head: ${s1.head}")
+    println(s"s1.headOption.getOrElse(Empty Seq): ${s1.headOption.getOrElse("Empty Seq")}")
+    println(s"s2.headOption.getOrElse(Empty Seq): ${s2.headOption.getOrElse("Empty Seq")}")
     println("====================== ====== ======================")
   }
 }
