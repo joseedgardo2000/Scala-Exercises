ad5ff6916646d2368aacfda2f123f20176175d3d
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-13 16:02:17 -0600

    Intersect - Init
    Intersect - Init

--- CollectionFunctions/Intersect/src/main/scala/Intersect.scala
+++ CollectionFunctions/Intersect/src/main/scala/Intersect.scala
@@ -1,14 +1,14 @@
 
 
-object Diff {
+object Intersect {
   def main(args:Array[String]):Unit = {
-    println("====================== Diff ======================")
+    println("====================== Intersect ======================")
     val s1 = Seq("pig",  "horse", "cow",     "monkey", "fly")
     val s2 = Seq("wolf", "horse", "octopus", "mouse",   "fly")
     println(s1)
     println(s2)
-    println("s1.diff(s2): " + s1.diff(s2))
-    println("s2.diff(s1): " + s2.diff(s1))
+    println("s1.intersect(s2): " + s1.intersect(s2))
+    println("s2.intersect(s1): " + s2.intersect(s1))
     println("====================== ====== ======================")
   }
 }
