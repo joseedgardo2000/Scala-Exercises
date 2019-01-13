bbbd6cf0ff19b55d344bf6fa03d35340ed463058
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-13 16:34:51 -0600

    Tail - Init
    Tail - Init

---
+++ CollectionFunctions/Tail/build.sbt
@@ -0,0 +1,3 @@
+name:="Tail"
+version:="1.0"
+scalaVersion:="2.12.7"
---
+++ CollectionFunctions/Tail/src/main/scala/Tail.scala
@@ -0,0 +1,11 @@
+
+
+object Tail {
+  def main(args:Array[String]):Unit = {
+    println("====================== Tail ======================")
+    val s1 = Seq("pig", "horse", "cow", "monkey", "fly")
+    println(s1)
+    println(s1.tail)
+    println("====================== ====== ======================")
+}
+}
