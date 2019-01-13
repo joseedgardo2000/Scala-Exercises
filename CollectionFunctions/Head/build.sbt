b78042adcfa15afa81cb43e0af193ab3a959fc9e
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-01-13 02:36:30 -0600

    Head - Init
    Head - Init

---
+++ CollectionFunctions/Head/build.sbt
@@ -0,0 +1,3 @@
+name:="Head"
+version:="1.0"
+scalaVersion:="2.12.7"
--- CollectionFunctions/Head/src/main/scala/Head.scala
+++ CollectionFunctions/Head/src/main/scala/Head.scala
@@ -1,12 +1,12 @@
 
 
-object Last {
+object Head {
   def main(args:Array[String]):Unit = {
-    println("====================== Last ======================")
+    println("====================== Head ======================")
     val s1 = Seq("pig", "horse", "cow", "monkey", "fly")
     println(s1)
-    println(s"s1(s1.size-1): ${s1(s1.size-1)}")
-    println(s"s1.last: ${s1.last}")
+    println(s"s1(0): ${s1(0)}")
+    println(s"s1.head: ${s1.head}")
     println("====================== ====== ======================")
   }
 }
